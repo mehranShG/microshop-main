175b2e7004946d148f4501859d5683bce8bde770
mehranShG <mehranfmpx@gmail.com>
2022-10-16 23:09:21 +0330

    update: schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_MONGO),
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
