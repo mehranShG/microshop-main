d2606ed84cb93f6b58f2faaa1c29b675fea260bd
mehranShG <mehranfmpx@gmail.com>
2022-10-18 20:12:10 +0330

    feature: swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { config } from 'dotenv'
 import { NestFactory } from '@nestjs/core'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  // Starting RabbitMQ microservice
   const microservice =
     await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
       transport: Transport.RMQ,
@@ -19,6 +20,16 @@ async function bootstrap() {
       },
     })
   microservice.listen()
+
+  // Loading Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Microshop')
+    .setDescription('Shop api swagger')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(8001)
   console.log('server started at %s', await app.getUrl())
 }
