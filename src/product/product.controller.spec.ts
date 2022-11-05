c7301c04dad2025452bd731710356fa8a45ad1c4
mehranShG <mehranfmpx@gmail.com>
2022-11-05 17:43:25 +0330

    update: controller create product  unit testing

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { CreateDto } from '../dtos/product.dto'
 import { ProductController } from './product.controller'
 import { ProductService } from './product.service'
 
 const mockProductService = {
   getAll: jest.fn().mockReturnValue([1, 2, 3, 4]),
+  create: jest.fn().mockResolvedValue({ id: 1, title: 'tree.png' }),
 }
 
 describe('ProductController', () => {
@@ -28,6 +30,16 @@ describe('ProductController', () => {
   describe('getAll', () => {
     it('should be defined', () => {
       expect(controller.getAll()).toEqual([1, 2, 3, 4])
+    })
+  })
+
+  describe('createProudct', () => {
+    it('should create product', async () => {
+      const product = new CreateDto()
+      expect(await controller.productCreate(product)).toEqual({
+        id: 1,
+        title: 'tree.png',
+      })
     })
   })
 })
