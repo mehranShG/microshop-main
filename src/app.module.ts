46b2ae05db48fa65e9bcd6f81dafbbbd0074118c
mehranShG <mehranfmpx@gmail.com>
2022-11-10 20:16:17 +0330

    update: file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
-import { AuthModule } from './auth/auth.module'
+import { CartModule } from './cart/cart.module'
 import { ProductModule } from './product/product.module'
-import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_MONGO),
     ProductModule,
-    AuthModule,
     CartModule,
   ],
 })
