a0bfb5c2cb472ec8615a8d3010db3f03be1ef687
mehranShG <mehranfmpx@gmail.com>
2022-11-23 21:23:29 +0330

    feature: cart resource added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ProductModule } from './product/product.module'
 import { UserModule } from './user/user.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './user/user.module';
     MongooseModule.forRoot(process.env.DB_MONGO),
     ProductModule,
     UserModule,
+    CartModule,
   ],
 })
 export class AppModule {}
