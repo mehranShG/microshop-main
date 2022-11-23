cbf70bdd9cbaa3570ebe12ca7ebd433927b58c74
mehranShG <mehranfmpx@gmail.com>
2022-11-23 21:21:24 +0330

    user resource added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ProductModule } from './product/product.module'
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_MONGO),
     ProductModule,
+    UserModule,
   ],
 })
 export class AppModule {}
