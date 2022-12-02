3ca6124f6bed5821de2bd09f49e1b51ea64f4acf
mehranShG <mehranfmpx@gmail.com>
2022-12-02 19:04:45 +0330

    update: structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ProductModule } from './product/product.module'
-import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_MONGO),
     ProductModule,
-    UserModule,
   ],
 })
 export class AppModule {}
