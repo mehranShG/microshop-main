ca7ddfd72f594309955e1c425b8307a0963d2458
mehranShG <mehranfmpx@gmail.com>
2022-12-05 21:04:58 +0330

    update: registerDto class-validator

--- src/dtos/register.dto.ts
+++ src/dtos/register.dto.ts
@@ -1,12 +1,24 @@
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
 
 export class RegisterDto {
   @ApiProperty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z\-]+$/, { message: 'username should Not have any digits' })
   username: string
 
   @ApiProperty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'password must contain atleast one capital letter',
+  })
   password: string
 
   @ApiProperty()
+  @IsEmail()
   email: string
 }
