c6795db525174b2b5224fbcac3f56f20c1a0f38c
mehranShG <mehranfmpx@gmail.com>
2022-12-05 20:46:13 +0330

    feature: auth RegisterDto added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import { RegisterDto } from 'src/dtos/register.dto'
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 
@@ -6,7 +7,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  Register(@Body() registerDto) {
+  Register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto)
   }
 }
