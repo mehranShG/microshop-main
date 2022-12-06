99938bf04f3529f2bb68826b9344470d65ddad8b
mehranShG <mehranfmpx@gmail.com>
2022-12-06 18:20:04 +0330

    update: authcontroller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
 import { RegisterDto } from 'src/dtos/register.dto'
+import { AuthPass } from 'src/entities/auth-pass.entity'
+import { AuthEntity } from 'src/entities/auth.entity'
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 
@@ -8,6 +10,12 @@ export class AuthController {
 
   @Post()
   Register(@Body() registerDto: RegisterDto) {
-    return this.authService.register(registerDto)
+    const user = new AuthEntity()
+    user.username = registerDto.username
+    user.email = registerDto.email
+    const authPass = new AuthPass()
+    authPass.password = registerDto.password
+    user.authPass = authPass
+    return this.authService.register(user)
   }
 }
