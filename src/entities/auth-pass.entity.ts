665074ab91e027cfafcf9914598a72c93fc53575
mehranShG <mehranfmpx@gmail.com>
2022-12-06 18:43:50 +0330

    feature: bcrypt added

--- src/entities/auth-pass.entity.ts
+++ src/entities/auth-pass.entity.ts
@@ -1,4 +1,8 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import * as bcrypt from 'bcrypt'
+import {
+    BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm'
 
 @Entity()
 export class AuthPass {
@@ -13,4 +17,11 @@ export class AuthPass {
 
   @UpdateDateColumn()
   updated_at: Date
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+  }
 }
