f4eaf9bcd006fd2cba7709412623e48245328bf4
mehranShG <mehranfmpx@gmail.com>
2022-12-07 16:48:36 +0330

    feature: jwt added

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { validate } from 'class-validator'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { AuthService } from './auth.service'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: '1234',
+    })
+  }
+  async validate(payload: any) {
+    const user = await this.authService.findById(payload.id)
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return { user: user.id, username: user.username }
+  }
+}
