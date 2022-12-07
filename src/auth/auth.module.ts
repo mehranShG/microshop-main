9eb29786cba993c51011e3b98d1b9541dc1498a2
mehranShG <mehranfmpx@gmail.com>
2022-12-07 19:46:13 +0330

    update: env example

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { config } from 'dotenv'
 import { AuthPass } from 'src/entities/auth-pass.entity'
 import { AuthEntity } from 'src/entities/auth.entity'
 import { Module } from '@nestjs/common'
@@ -7,10 +8,12 @@ import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { JwtStrategy } from './jwt.strategy'
 
+config()
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([AuthEntity, AuthPass]),
-    JwtModule.register({ secret: '1234' }),
+    JwtModule.register({ secret: process.env.JWT_SECRET }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
