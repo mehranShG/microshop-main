e580244546fa670dd6bc6fede58fa735a8c379d9
mehranShG <mehranfmpx@gmail.com>
2022-12-10 13:22:00 +0330

    update: jwt.strategy env

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,18 @@
 import { validate } from 'class-validator'
+import { config } from 'dotenv'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { AuthService } from './auth.service'
+
+config()
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: '1234',
+      secretOrKey: process.env.JWT_SECRET,
     })
   }
   async validate(payload: any): Promise<Object> {
