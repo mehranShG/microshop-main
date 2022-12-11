88fee9639ea3ea630c58893617b646b310393562
mehranShG <mehranfmpx@gmail.com>
2022-12-11 20:03:18 +0330

    update: getUserProfile route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { ResponseModel } from 'src/interface/response.model'
+import { Controller, Get, Param } from '@nestjs/common'
+import { UserService } from './user.service'
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile:id')
+  async getUserProfile(@Param('id') id: number): Promise<ResponseModel> {
+    return this.userService.getUserProfile(id)
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { AuthEntity } from 'src/entities/auth.entity'
 import { ResponseModel } from 'src/interface/response.model'
 import { Repository } from 'typeorm'
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
@@ -11,8 +11,11 @@ export class UserService {
     private readonly AuthRepository: Repository<AuthEntity>,
   ) {}
 
-  async getUserProfile(id): Promise<ResponseModel> {
-    const findUser = await this.AuthRepository.findOne({ where: { id: 1 } })
+  async getUserProfile(id: number): Promise<ResponseModel> {
+    const findUser = await this.AuthRepository.findOne({ where: { id: id } })
+    if (!findUser) {
+      throw new NotFoundException()
+    }
     const profile = {
       id: findUser.id,
       first_name: findUser.first_name,
