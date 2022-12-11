7cd4d09d91878ce96e95be90b9b23ebcc24e0b22
mehranShG <mehranfmpx@gmail.com>
2022-12-11 19:50:28 +0330

    update: user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { AuthPass } from 'src/entities/auth-pass.entity'
+import { AuthEntity } from 'src/entities/auth.entity'
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserController } from './user.controller'
+import { UserService } from './user.service'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AuthEntity, AuthPass])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { AuthEntity } from 'src/entities/auth.entity'
+import { Repository } from 'typeorm'
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(AuthEntity)
+    private readonly AuthRepository: Repository<AuthEntity>,
+  ) {}
+}
