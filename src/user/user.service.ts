d4b4f62868dc4f3ed41658989f365bcc31f2a0d8
mehranShG <mehranfmpx@gmail.com>
2022-12-11 19:58:18 +0330

    feature: user getUserProfile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { AuthEntity } from 'src/entities/auth.entity'
+import { ResponseModel } from 'src/interface/response.model'
 import { Repository } from 'typeorm'
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
@@ -9,4 +10,21 @@ export class UserService {
     @InjectRepository(AuthEntity)
     private readonly AuthRepository: Repository<AuthEntity>,
   ) {}
+
+  async getUserProfile(id): Promise<ResponseModel> {
+    const findUser = await this.AuthRepository.findOne({ where: { id: 1 } })
+    const profile = {
+      id: findUser.id,
+      first_name: findUser.first_name,
+      last_name: findUser.last_name,
+      phone_number: findUser.phone_number,
+      address: findUser.address,
+      profile_image: findUser.profile_image,
+    }
+    return {
+      success: true,
+      result: profile,
+      code: 200,
+    }
+  }
 }
