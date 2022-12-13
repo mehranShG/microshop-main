7127fcc58a49e43a8b6a8a5f2e8ef7e9e4fde178
mehranShG <mehranfmpx@gmail.com>
2022-12-13 20:06:55 +0330

    update: user getProfile microservice controller

--- src/user/user-microservice.controller.ts
+++ src/user/user-microservice.controller.ts
@@ -1,5 +1,6 @@
-
-import { Controller } from '@nestjs/common'
+import { ResponseModel } from 'src/interface/response.model'
+import { Controller, Get } from '@nestjs/common'
+import { MessagePattern } from '@nestjs/microservices'
 import { ApiTags } from '@nestjs/swagger'
 import { UserService } from './user.service'
 
@@ -7,4 +8,10 @@ import { UserService } from './user.service'
 @Controller('microservice/user')
 export class UserMicroController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @MessagePattern({ name: 'get_user_profile' })
+  async getUserProfile(id: number): Promise<ResponseModel> {
+    return this.userService.getUserProfile(id)
+  }
 }
