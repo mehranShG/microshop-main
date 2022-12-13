1102263ba9ef260bea02011c20e6949db221a609
mehranShG <mehranfmpx@gmail.com>
2022-12-13 20:19:14 +0330

    update: user update Profile microservice controller

--- src/user/user-microservice.controller.ts
+++ src/user/user-microservice.controller.ts
@@ -1,5 +1,5 @@
 import { ResponseModel } from 'src/interface/response.model'
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Put } from '@nestjs/common'
 import { MessagePattern } from '@nestjs/microservices'
 import { ApiTags } from '@nestjs/swagger'
 import { UserService } from './user.service'
@@ -13,5 +13,11 @@ export class UserMicroController {
   @MessagePattern({ name: 'get_user_profile' })
   async getUserProfile(id: number): Promise<ResponseModel> {
     return this.userService.getUserProfile(id)
+  }
+
+  @Put('update')
+  @MessagePattern({ name: 'update_user_profile' })
+  async updateProfile(payload: any): Promise<ResponseModel> {
+    return this.userService.updateProfile(payload.id, payload.updateProfileDto)
   }
 }
