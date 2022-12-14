be99663a6df5436c30a9fc785dabd237a5cddb80
mehranShG <mehranfmpx@gmail.com>
2022-12-14 20:38:58 +0330

    update: user.service  getUserProfile  unit testing

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,7 +3,15 @@ import { getRepositoryToken } from '@nestjs/typeorm'
 import { AuthEntity } from '../entities/auth.entity'
 import { UserService } from './user.service'
 
-const MockAuthModel = {}
+const MockAuthModel = {
+  findOne: jest.fn().mockResolvedValue({
+    id: 1,
+    first_name: 'alex',
+    last_name: 'gold',
+    address: 'test street',
+    phone_number: '0911111111',
+  }),
+}
 describe('UserService', () => {
   let service: UserService
 
@@ -20,5 +28,22 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('getUserProfile', () => {
+    it('should get user profile', async () => {
+      expect(await service.getUserProfile(1)).toEqual({
+        success: true,
+        result: {
+          address: 'test street',
+          first_name: 'alex',
+          id: 1,
+          last_name: 'gold',
+          phone_number: '0911111111',
+          profile_image: undefined,
+        },
+        code: 200,
+      })
+    })
   })
 })
