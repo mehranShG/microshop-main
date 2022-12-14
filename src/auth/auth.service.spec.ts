9e5570d0db879215cf082af9a7fe7ca2c88460de
mehranShG <mehranfmpx@gmail.com>
2022-12-14 20:59:36 +0330

    update: auth.service test dependencies

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt'
+import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { AuthEntity } from '../entities/auth.entity'
+import { AuthService } from './auth.service'
 
+const fakeAuthEntity = {}
 describe('AuthService', () => {
-  let service: AuthService;
+  let service: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      providers: [
+        AuthService,
+        JwtService,
+        { provide: getRepositoryToken(AuthEntity), useValue: fakeAuthEntity },
+      ],
+    }).compile()
 
-    service = module.get<AuthService>(AuthService);
-  });
+    service = module.get<AuthService>(AuthService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
