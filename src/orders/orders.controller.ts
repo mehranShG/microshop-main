a6da89e722623f87213b82266a206e2f3fc5694f
mehranShG <mehranfmpx@gmail.com>
2022-12-19 21:17:52 +0330

    feature: orders getOrder

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import { Controller, Get } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { OrdersService } from './orders.service'
 
+@ApiTags('Orders')
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  getOrder() {
+    return this.ordersService.getOrder()
+  }
 }
