890ef0de8460670a99606278628ea554ec90e2b2
mehranShG <mehranfmpx@gmail.com>
2022-12-19 21:10:40 +0330

    update: order service dependencies

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
+import { AuthEntity } from 'src/entities/auth.entity'
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { OrdersController } from './orders.controller'
+import { OrdersService } from './orders.service'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AuthEntity])],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
 })
 export class OrdersModule {}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { AuthEntity } from 'src/entities/auth.entity'
+import { Repository } from 'typeorm'
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectRepository(AuthEntity)
+    private readonly authRepository: Repository<AuthEntity>,
+  ) {}
+}
