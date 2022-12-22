2684359de29634d75dab77276e8193724d270ee3
mehranShG <mehranfmpx@gmail.com>
2022-12-22 22:55:40 +0330

    update: documentation

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -93,6 +93,37 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 </li>
                             </li>
                             <li class="link">
+                                <a href="modules/FinanceModule.html" data-type="entity-link" >FinanceModule</a>
+                                    <li class="chapter inner">
+                                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                            'data-target="#controllers-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' : 'data-target="#xs-controllers-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' }>
+                                            <span class="icon ion-md-swap"></span>
+                                            <span>Controllers</span>
+                                            <span class="icon ion-ios-arrow-down"></span>
+                                        </div>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="controllers-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' :
+                                            'id="xs-controllers-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' }>
+                                            <li class="link">
+                                                <a href="controllers/FinanceController.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >FinanceController</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                <li class="chapter inner">
+                                    <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                        'data-target="#injectables-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' : 'data-target="#xs-injectables-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' }>
+                                        <span class="icon ion-md-arrow-round-down"></span>
+                                        <span>Injectables</span>
+                                        <span class="icon ion-ios-arrow-down"></span>
+                                    </div>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' :
+                                        'id="xs-injectables-links-module-FinanceModule-57368c2b6ca3faf048bf8b92e6ceeda6cc00ae02f0075be85d9761ebab9cf784899414abc66df9a1b4963efb267fd1c77a4215449e5cf2352fa53fc782e78036"' }>
+                                        <li class="link">
+                                            <a href="injectables/FinanceService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >FinanceService</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </li>
+                            <li class="link">
                                 <a href="modules/OrdersModule.html" data-type="entity-link" >OrdersModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
