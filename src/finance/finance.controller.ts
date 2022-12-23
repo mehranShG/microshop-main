a917eadfe75fb1b587408969898a7eab3fe1f66d
mehranShG <mehranfmpx@gmail.com>
2022-12-23 21:56:10 +0330

    update: structre

--- src/finance/finance.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { FinanceService } from './finance.service';
-
-@Controller('finance')
-export class FinanceController {
-  constructor(private readonly financeService: FinanceService) {}
-}
